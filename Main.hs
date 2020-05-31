4472526283bafd992bec0071fd8040251b04f8df
Oleg Musijenko <45047700+Poselsky@users.noreply.github.com>
2020-05-31 21:02:05 +0200

    add better example

--- Main.hs
+++ Main.hs
@@ -28,7 +28,7 @@ pitchStandard :: Hz
 pitchStandard = 440.0
 
 bpm :: Beats
-bpm = 90.0
+bpm = 160
 
 beatDuration :: Seconds
 beatDuration = 60.0 / bpm
@@ -53,8 +53,8 @@ freq hz duration =
     adsrRatio:: Int -> (Float, Float)
     adsrRatio index = (width !! index, atan $ ((height !! (index + 1)) - (height !! index)) / ((width !! (index + 1)) - (width !! (index))))
       where
-        width = zipWith (\x len -> (x / len):: Float) [15,10,55,20,0] $ repeat $ int2Float outputLength
-        height = [0.0,1.0, 0.6, 0.6, 0]
+        width = zipWith (\x len -> (x / len):: Float) [15,5,60,20,0] $ repeat $ int2Float outputLength
+        height = [0.0,1.0, 0.75, 0.75, 0]
 
     outputLength:: Int
     outputLength = length output
@@ -72,86 +72,6 @@ freq hz duration =
               index = int2Float ind
               pointStep = 1 / (int2Float (outputLength))
 
-
-adsr:: [Float]
-adsr = fst $ foldr foldHelper ([], 0.0) $ zip [0..] $ take 10 $ repeat 1
-    where
-      foldHelper:: (Int, Float) -> ([Float], Float) -> ([Float], Float)
-      foldHelper (ind, val) (accList, previousVal)
-        | index <= (fst $ adsrRatio 0)                                 = ([previousVal + (snd $ adsrRatio 0)] ++ accList, previousVal + (snd $ adsrRatio 0))
-        | index >  (fst $ adsrRatio 0) && index <= (fst $ adsrRatio 1) = ([previousVal - (snd $ adsrRatio 1)] ++ accList, previousVal - (snd $ adsrRatio 1))
-        | index >  (fst $ adsrRatio 1) && index <= (fst $ adsrRatio 2) = ([0.60] ++ accList,0.60)
-        | index >  (fst $ adsrRatio 2)                                 = ([previousVal - (snd $ adsrRatio 2)] ++ accList, previousVal - (snd $ adsrRatio 2))
-          where index = int2Float ind
-      -- Ratio and steepnes
-      adsrRatio:: Int -> (Float, Float)
-      adsrRatio index = (adsrRatio' !! index, (adsrRatio' !! (index + 1)) / ((adsrRatio' !! (index + 1)) - (adsrRatio' !! (index))))
-        where
-          adsrRatio' = zipWith (\x len -> (x / len):: Float) [25,15,40,20] $ repeat $ int2Float 10
-
-awaken :: [Pulse]
-awaken =
-  concat
-    [ 
-        noteF5 0.5
-      , noteF5 0.25
-      , noteF5 0.25
-      , noteF5 0.25
-      , noteD5Eb5 0.25
-      , noteF5 0.25
-      , noteF5Gb5 0.25
-      , noteG5Ab5 0.25
-      , noteA5 0.25
-      , noteG5Ab5 0.25
-      , noteF5Gb5 0.25
-      , noteF5 0.5
-      , noteF5 0.5
-
-      , noteZero 0.5
-
-      , noteF5 0.5
-      , noteF5 0.25
-      , noteF5 0.25
-      , noteF5 0.25
-      , noteD5Eb5 0.25
-      , noteF5 0.25
-      , noteF5Gb5 0.25
-      , noteG5Ab5 0.25
-      , noteA5 0.25
-      , noteG5Ab5 0.25
-      , noteF5Gb5 0.25
-      , noteF5 0.5
-      , noteD5Eb5 0.5
-
-      , noteZero 0.5
-
-      , noteF5 0.25
-      , noteF5 0.25
-      , noteF5 0.25
-      , noteD5Eb5 0.25
-      , noteF5 0.25
-      , noteF5Gb5 0.25
-      , noteG5Ab5 0.25
-      , noteA5 0.25
-      , noteG5Ab5 0.25
-      , noteF5Gb5 0.25
-      , noteF5 0.5
-      , noteF5 0.5
-      , noteF5 0.25
-      , noteF5 0.5
-      , noteF5Gb5 0.25
-      , noteF5Gb5 0.5
-      , noteG5Ab5 0.25
-      , noteG5Ab5 0.25
-      , noteZero 0.25
-      , noteA5 0.25
-      , noteA5 0.5
-      , noteC5 0.25
-      , noteA5 0.25
-      , noteG5Ab5 0.25
-      , noteF5Gb5 0.25
-
-    ]
 
 hehehe :: [Pulse]
 hehehe = concat [ note 0 0.25
@@ -176,8 +96,148 @@ hehehe = concat [ note 0 0.25
                 , note 5 0.25
                 ]
 
+testingBeats:: [Pulse]
+testingBeats = zipWith (+) (map (0.7*)firstPart) (map (0.3*) secondPart)
+  where
+    firstPart = concat [
+                        noteZero 0.5
+                        , noteG4 0.25
+                        , noteG4 0.5
+                        , noteG4 0.5
+                        , noteF4 0.5
+                        , noteG4 0.5
+                        , noteA4 0.5
+                        , noteB4 0.5
+                        , noteB4 0.5
+                        , noteA4Bb4 0.5
+                        , noteA4 0.5
+                        , noteG4 0.25
+                        , noteZero 0.25
+                        , noteZero 0.5
+                        , noteG4 0.125
+                        , noteZero 0.125
+                        
+                        , noteZero 0.5
+                        , noteZero 0.25
+                        
+                        , noteG4 0.125
+                        , noteZero 0.125
+                        , noteG4 0.25
+                        , noteZero 0.25
+                        , noteG4 0.25
+                        , noteZero 0.25
+                        , noteF4 0.25
+                        , noteZero 0.25
+                        , noteG4 0.25
+                        , noteZero 0.25
+                        , noteA4 0.25
+                        , noteZero 0.25
+                        , noteB4 0.25
+                        , noteZero 0.25
+                        , noteB4 0.25
+                        , noteZero 0.25
+                        , noteA4Bb4 0.25
+                        , noteZero 0.25
+                        , noteA4 0.25
+                        , noteZero 0.25
+                        , noteG4 0.25
+                        , noteZero 0.25
+                        , noteZero 0.5
+                        , noteF4 0.125
+                        , noteZero 1.25
+
+                        , noteZero 0.5
+                        , noteG4 0.125
+                        , noteZero 0.125
+                        , noteG4 0.25
+                        , noteZero 0.25
+                        , noteG4 0.25
+                        , noteZero 0.25
+                        , noteF4 0.25
+                        , noteZero 0.25
+                        , noteG4 0.25
+                        , noteZero 0.25
+                        , noteA4 0.25
+                        , noteZero 0.25
+                        , noteB4 0.25
+                        , noteZero 0.25
+                        , noteB4 0.25
+                        , noteZero 0.25
+                        , noteA4Bb4 0.25
+                        , noteZero 0.25
+                        , noteA4 0.25
+                        , noteZero 0.25
+                        , noteG4 0.25
+                        , noteZero 0.25
+                        , noteZero 0.5
+                        , noteG4 0.125
+                        , noteZero 0.125
+                        
+
+                        , noteZero 0.5
+
+                        , noteG4 0.25
+                        , noteZero 0.25
+                        , noteA4 0.25
+                        , noteZero 0.25
+                        , noteA4 0.25
+                        , noteZero 0.25
+                        , noteB4 0.125
+                        , noteZero 0.125
+                        , noteB4 0.25
+                        , noteZero 0.25
+                        , noteB4 0.25
+                        , noteZero 0.25
+                        , noteZero 0.25
+                        , noteD5 0.25
+                        , noteZero 0.25
+                        , noteB4 0.25
+                        , noteZero 0.25
+                        , noteA4Bb4 0.25
+                        , noteZero 0.25
+
+                        , noteA4 0.25
+                        , noteZero 0.25
+                      ]
+    secondPart = concat [
+                          noteZero 0.25
+                          , noteG4 3
+                          , noteG5 0.25
+                          , noteG5 0.25
+
+                          , noteD5 3
+                          , noteE5 0.25
+                          , noteE5 0.25
+                          , noteD5 3
+
+                          , noteB4 0.25
+                          , noteB4 0.25
+                          , noteA4Bb4 3
+
+                          , noteA4 0.25
+                          , noteA4 0.25
+
+                          , noteG4 3
+                          , noteG5 0.25
+                          , noteG5 0.25
+
+                          , noteD5 3
+                          , noteE5 0.25
+                          , noteE5 0.25
+                          , noteD5 3
+
+                          , noteB4 0.25
+                          , noteB4 0.25
+                          , noteA4Bb4 3
+
+                          , noteA4 0.25
+                          , noteA4 0.25
+
+                          , repeat 0
+                        ]
+
 save :: FilePath -> IO ()
-save filePath = B.writeFile filePath $ B.toLazyByteString $ fold $ map B.floatLE awaken
+save filePath = B.writeFile filePath $ B.toLazyByteString $ fold $ map B.floatLE testingBeats
 
 play :: IO ()
 play = do
@@ -212,4 +272,47 @@ noteA5:: Seconds -> [Pulse]
 noteA5 beats = freq (f 12) (beats * beatDuration)
 
 noteC5:: Seconds -> [Pulse]
-noteC5 beats = freq (f 15) (beats * beatDuration)
+noteC5 beats = freq (f 3) (beats * beatDuration)
+
+noteG5:: Seconds -> [Pulse]
+noteG5 beats = freq (f 10) (beats * beatDuration)
+
+
+noteD5:: Seconds -> [Pulse]
+noteD5 beats = freq (f 5) (beats * beatDuration)
+
+noteE5:: Seconds -> [Pulse]
+noteE5 beats = freq (f 7) (beats * beatDuration)
+
+noteC5Db5:: Seconds -> [Pulse]
+noteC5Db5 beats = freq (f 4) (beats * beatDuration)
+
+
+
+noteF4:: Seconds -> [Pulse]
+noteF4 beats = freq (f (-4)) (beats * beatDuration)
+
+noteD4Eb4:: Seconds -> [Pulse]
+noteD4Eb4 beats = freq (f (-6)) (beats * beatDuration)
+
+noteF4Gb4:: Seconds -> [Pulse]
+noteF4Gb4 beats = freq (f (-3)) (beats * beatDuration)
+
+noteG4Ab4:: Seconds -> [Pulse]
+noteG4Ab4 beats = freq (f (-1)) (beats * beatDuration)
+
+noteA4:: Seconds -> [Pulse]
+noteA4 beats = freq (f 0) (beats * beatDuration)
+
+noteB4:: Seconds -> [Pulse]
+noteB4 beats = freq (f 2) (beats * beatDuration)
+
+noteG4:: Seconds -> [Pulse]
+noteG4 beats = freq (f (-2)) (beats * beatDuration)
+
+noteC4:: Seconds -> [Pulse]
+noteC4 beats = freq (f (-9)) (beats * beatDuration)
+
+
+noteA4Bb4:: Seconds -> [Pulse]
+noteA4Bb4 beats = freq (f 1) (beats * beatDuration)
